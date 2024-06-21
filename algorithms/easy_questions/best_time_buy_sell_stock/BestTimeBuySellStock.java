b65a79e292fa5f5f7989d5fa731edef6bae3a792
Ethan <smitethan0302@gmail.com>
2024-06-21 19:35:54 -0400

    Fixed naming.

--- algorithms/easy_questions/best_time_buy_sell_stock/BestTimeBuySellStock.java
+++ algorithms/easy_questions/best_time_buy_sell_stock/BestTimeBuySellStock.java
@@ -2,21 +2,21 @@ package algorithms.easy_questions.best_time_buy_sell_stock;
 
 public class BestTimeBuySellStock {
     public int maxProfit(int[] prices) {
-        int max_profit = 0;
-        int start_day = 0;
+        int maxProfitVar = 0;
+        int startDay = 0;
 
         for (int i = 1; i < prices.length; i++) {
             // If the difference in prices between the current - start day is greater than max_profit,
             // then this is our new largest possible profit.
-            if (prices[i] - prices[start_day] > max_profit) {
-                max_profit = prices[i] - prices[start_day];
-            } else if (prices[i] < prices[start_day]) { 
+            if (prices[i] - prices[startDay] > maxProfitVar) {
+                maxProfitVar = prices[i] - prices[startDay];
+            } else if (prices[i] < prices[startDay]) { 
                 // Choose lowest price day to ensure maximum possible profit.
-                start_day = i;
+                startDay = i;
             }
         }
         // Answer
-        return max_profit;
+        return maxProfitVar;
     }
 }
 
